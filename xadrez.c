8579dfa5ae3710f40c518a7c5dc16a3e2fcd3446
marcelooliverdev <marceloelias776@gmail.com>
2026-04-29 18:14:25 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,12 +1,67 @@
 #include <stdio.h>
+
+
+     
+    void MoverTorre(int casas) // mover torre void
+    {
+
+        if (casas == 0)
+    
+            return;
+         
+
+            printf("Direita\n");
+            MoverTorre(casas - 1);
+        
+    }
+
+
+    void MoverBispo(int casas){ //mover bispo void
+        
+            if (casas == 0)
+            
+                return;
+
+                    for (int b = 0; b < 1 ; b++) // vertical
+                    {
+                        printf("Cima\n");
+
+                    for (int j = 0; j < 1; j++) // horizontal
+                    {
+                        printf("Direita\n");
+                    }
+                    
+
+                    }
+                    
+            MoverBispo(casas - 1);
+        
+    }
+
+
+
+        void MoverRainha (int casas){
+
+             
+            if(casas == 0)
+
+        return;
+
+                printf("Esquerda\n");
+                MoverRainha(casas - 1);
+
+
+        }
+
+
+
 int main () {
+
     // mover a torre
     printf("\n Movimentacao da Torre! \n");
-
-        for (int t = 0; t < 5; t++)
-        {
-            printf("Direita\n");
-        }
+         
+            MoverTorre(5);
+              
 
         printf("\n"); // para espaco!
 
@@ -14,27 +69,20 @@ int main () {
     // mover o bispo 
      printf("\n Movimentacao do Bispo! \n");
 
-    int b = 0;
-    while (b < 5)
-    {
-        
-        printf("Cima, Direita\n");
-    b++;
-    }
+     MoverBispo(5);
+     
+
+   
 
      printf("\n"); // para espaco!
     
 
     // mover a rainha 
     printf("\n Movimentacao da Rainha! \n");
+    MoverRainha(8);
+    
 
-    int r = 0;
-    do
-    {
-        printf("Esquerda\n");
-        r++;
-    } while (r < 8);
-
+   
      printf("\n"); // para espaco!
 
 
@@ -42,23 +90,22 @@ int main () {
 
     printf("\n Movimentacao do Cavalo!\n");
 
-
-    int movimento = 1;
-
-    while (movimento--)
+    for (int movimento = 0; movimento < 1; movimento++)
+{
+    for (int cima = 0; cima < 2; cima++)
     {
+        printf("Cima\n");
+    }
 
-        for (int i = 0; i < 2; i++)
-        {
-            printf("Baixo\n");
-
-
-        }
-
-        printf("Esquerda\n");
-
-         printf("\n"); // para espaco!
+    printf("Direita\n");
 }
+            
+
+    
+    
+
+
+   
     
 
 
